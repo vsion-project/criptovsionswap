cc5f109811dd9a0e79310a9752aeff3c21b810dd
fernandoum <fernanditososa@gmail.com>
2021-08-19 18:38:57 -0300

    index

--- src/App.tsx
+++ src/App.tsx
@@ -78,9 +78,9 @@ const App: React.FC = () => {
             <Route path="/pools">
               <Pools />
             </Route>
-//            <Route path="/lottery">
-//              <Lottery />
-//            </Route>
+            <Route path="/lottery">
+              <Lottery />
+            </Route>
             <Route path="/ifo">
               <Ifos />
             </Route>
